606191f20469b9d667903e122fbfeefab537c92c
Richard W <richard@witjaksono.com>
2016-04-24 23:00:19 +1200

    Hello CodeShare!

---
+++ CodeShare/Custom/WebParts/Content/Content.ascx
@@ -0,0 +1,3 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="Content.ascx.cs" Inherits="Custom_WebParts_Content_Content" %>
+
+<asp:Literal ID="Content" runat="server"></asp:Literal>
---
+++ CodeShare/Custom/WebParts/Content/Content.ascx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using CMS.PortalControls;
+using CMS.Helpers;
+
+public partial class Custom_WebParts_Content_Content : CMSAbstractWebPart
+{
+
+	public string Description
+	{
+		get
+		{
+			return ValidationHelper.GetString(GetValue("Description"), String.Empty);
+		}
+		set
+		{
+			SetValue("Description", value);
+		}
+	}
+
+
+	/// <summary>
+	/// Content loaded event handler.
+	/// </summary>
+	public override void OnContentLoaded()
+	{
+		base.OnContentLoaded();
+		SetupControl();
+	}
+
+
+	private void SetupControl()
+	{
+		if (!Description.Trim().StartsWith("<"))
+		{
+			Description = "<p>" + Description + "</p>";
+		}
+
+		Content.Text = String.Format("<div class='content-section editor-content'>{0}</div>", Description);
+	}
+
+	protected void Page_Load(object sender, EventArgs e)
+	{
+
+	}
+}
